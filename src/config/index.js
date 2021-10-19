4de88ca27179fbcf7c0b8884bfb2dd176a4da1ea
Alexon1999 <alexon1999@gmail.com>
2021-10-19 21:34:55 +0200

    ok

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
-const apiUrl = "https://willtv.herokuapp.com";
+const apiUrl = "http://localhost:8000";
 const stripe_public_key =
-  "pk_live_51J8GK0C1J1Je91FHQmbQVkUJkYSrAv5LAnbQ7BFq0PWRskmbKO4DZE3w4avbNZSYasmGNHBG9zqSXrz1dBiuMbcM00KtYYLh4Y";
+  "pk_test_51J8GK0C1J1Je91FHTDijM2WyeJw9O5WyISlB7lvgcotxRvrGE7VNi13dV0xw3h36PXszgSYIWGLRHXhO06LKwoDw00AWrsQ4oK";
 
 export { apiUrl, stripe_public_key };
