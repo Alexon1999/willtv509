66987aff613287a4d17c1f2bc3e1f973f394878d
Alexon1999 <alexon1999@gmail.com>
2021-10-21 20:30:28 +0200

    ok

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
-const apiUrl = "https://willtv.herokuapp.com/";
+const apiUrl = "https://willtv.herokuapp.com";
 const stripe_public_key =
   "pk_live_51J8GK0C1J1Je91FHQmbQVkUJkYSrAv5LAnbQ7BFq0PWRskmbKO4DZE3w4avbNZSYasmGNHBG9zqSXrz1dBiuMbcM00KtYYLh4Y";
 
